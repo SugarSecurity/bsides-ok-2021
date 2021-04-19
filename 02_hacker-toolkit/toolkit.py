c681cd0d698b716431d64d6873882eed002b44e0
sweetroll <sweetrollbandit@protonmail.com>
2021-04-19 12:58:21 -0500

    commit rq

---
+++ 02_hacker-toolkit/toolkit.py
@@ -0,0 +1,21 @@
+import json
+
+
+def get_dashboard(event, context):
+    whoami
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+    # Use this code if you don't use the http event with the LAMBDA-PROXY
+    # integration
+    """
+    return {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "event": event
+    }
+    """
