23b6057827ebc6c55e7b10dc31797edbb8506d49
sweetroll <sweetrollbandit@protonmail.com>
2021-04-21 19:27:27 -0500

    oopsie

--- 02_hacker-toolkit/toolkit.py
+++ 02_hacker-toolkit/toolkit.py
@@ -5,11 +5,9 @@ import requests
 def get_osint(event, context):
     domain_api_input = event['pathParameters']['domain']
     whois_results = whois.whois(domain_api_input)
-    subdomain_results = requests.get(f"https://api.hackertarget.com/hostsearch?q={domain_api_input}/").content.decode("utf-8")
+    subdomain_results = requests.get(f"https://api.hackertarget.com/hostsearch?q={domain_api_input}").content.decode("utf-8")
     http_response = {
         "statusCode": 200,
         "body": json.dumps({"whois": whois_results, "subdomains": subdomain_results}, default=str)
     }
     return http_response
-
-    
